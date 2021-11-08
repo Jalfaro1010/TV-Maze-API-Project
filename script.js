47367bbd16da886faa8eab5d21c53561f6ed6476
BenW00 <ispyb2000@gmail.com>
2021-11-08 18:36:18 -0500

    made function use a paramter

--- script.js
+++ script.js
@@ -7,43 +7,43 @@ const form = document.querySelector(`form`)
 form.addEventListener(`submit` , () => {
     event.preventDefault();
     const show = form.elements.movie.value
-    getShow()
+    getShow(show)
 
 
-    async function getShow() {
-        try {
-        searchURL = baseURL + search + show
-        let response = await axios.get(searchURL)
-        Data = response.data
-        contain.innerHTML = ""
+})
+async function getShow (ShowNae) {
+    try {
+    searchURL = baseURL + search + ShowNae
+    let response = await axios.get(searchURL)
+    Data = response.data
+    contain.innerHTML = ""
 
-        for (set in Data) {
-            console.log(set)
-            if (Data[set].show[`image`] == null) {
-                continue;
-            }
-            else {
-
-            const h3 = document.createElement(`h3`)
-            const div = document.createElement(`div`)
-            const img = document.createElement(`img`)
-    
-            img.src = Data[set].show[`image`].medium
-            h3.innerText = Data[set].show[`name`]
-    
-            div.classList.add(`movie-card`)
-            
-    
-            div.append(img)
-            div.append(h3)
-            contain.append(div)
-    
-            }
+    for (set in Data) {
+        console.log(set)
+        if (Data[set].show[`image`] == null) {
+            continue;
         }
-        }
-    
-        catch (err){
-            console.log(err)
+        else {
+
+        const h3 = document.createElement(`h3`)
+        const div = document.createElement(`div`)
+        const img = document.createElement(`img`)
+
+        img.src = Data[set].show[`image`].medium
+        h3.innerText = Data[set].show[`name`]
+
+        div.classList.add(`movie-card`)
+        
+
+        div.append(img)
+        div.append(h3)
+        contain.append(div)
+
         }
     }
-})
+    }
+
+    catch (err){
+        console.log(err)
+    }
+}
